37e02587a60e8605bd0931f9369cbc19be86088f
Bookgin <dongsheoil@gmail.com>
2016-05-14 14:52:35 +0800

    mobile web

---
+++ docs/examples/navbar/tmp.js
@@ -0,0 +1,426 @@
+
+$('#info').show();
+
+$(document).ready(function() {
+	$('#1fork').click(function() {
+		$('#info').show();
+		$('#staff').hide();
+document.getElementById("intro").innerHTML = "聽見了路邊的歌聲，我又想起了那一天<br>
+還記得那個秋季的下午<br>
+我們抱著吉他 唱著歌<br>
+時間彷彿在那一剎那凝結<br>
+好久以前的故事了啊......<br>
+看著塵封已久的吉他 <br>
+我清一清灰塵 拎起了它<br>
+「走！我們上之夜吧」<br>
+她這麼燦爛地說著。";
+document.getElementById("performer").innerHTML = "
+毛鈺翔<br> 
+甯芝蓶<br>
+王佑安<br>
+羅凱齡<br>
+陳家棋<br>
+鄭開元<br>
+楊舒瑄<br>
+陳力榕<br>
+王昱舒<br>
+林政豪<br>
+林彥彤<br>
+王郁婷<br>
+林承豫<br>
+ ";
+// document.getElementById("bgimg").style = "background-image:url(http://media02.hongkiat.com/ww-flower-wallpapers/roundflower.jpg);";
+	});
+
+	$('#3fork').click(function() {
+		$('#info').show();
+		$('#staff').hide();
+document.getElementById("intro").innerHTML = "『欸欸你們是在大聲什麼啦?』<br>『我們不是在大聲啦, 我們是大 三 ⺠ 謠。』";
+document.getElementById("performer").innerHTML = "
+謝其良<br> 
+陳璽安<br>
+王思傑<br>
+林祐廷<br>
+賴廷威<br>
+陳思憫<br>
+王浩恩<br>
+";
+	});
+
+	$('#maledance').click(function() {
+		$('#info').show();
+		$('#staff').hide();
+document.getElementById("intro").innerHTML = "帥的男舞....<br>
+你嫌我寫太少 男人就該默默的跳舞";
+document.getElementById("performer").innerHTML = "
+徐嘉琪<br> 
+張以白<br>
+鄭筱樺<br>
+陳弘梵<br>
+許家豪<br>
+林良翰<br>
+陳允中<br>
+周聖荃<br>
+謝致有<br>
+";
+	});
+
+	$('#aca').click(function() {
+		$('#info').show();
+		$('#staff').hide();
+document.getElementById("intro").innerHTML = "阿卡貝拉，即為純人聲無伴奏演唱。藉由純人聲，來讓音樂的情緒張力更加豐富。今天我們Pentatubbies帶來兩首歌，分別是Pentatonix的〈That’s Christmas to me〉以及〈Daft Punk〉。前者為Pentatonix的聖誕節創作歌曲，具安詳氣息。後者為Pentatonix將知名樂團Daft Punk的經典歌曲做成的組曲，曲風較為搖滾。希望今天這兩首歌，各位能夠喜歡。";
+document.getElementById("performer").innerHTML = "
+楊舒瑄<br> 
+王郁婷<br>
+李衡<br>
+謝唯安<br>
+王昱舒<br>
+陳宥嘉<br>
+王佑安<br>
+陳弘梵<br> 
+";
+	});
+
+	$('#olddrama').click(function() {
+		$('#info').show();
+		$('#staff').hide();
+document.getElementById("intro").innerHTML = "今年不一定有老人劇<br>
+  有也不一定好笑<br>
+  <a href=\"https://goo.gl/AczYds\"> FB粉絲專頁</>";
+document.getElementById("performer").innerHTML = "
+林傳恩<br> 
+黃孝天<br>
+丁名初<br>
+李穆竺<br>
+陳柏佑<br>
+陳蕙心<br>
+王欣維<br>
+鄭岱昇<br>
+林之宇<br>
+";
+	});
+
+	$('#1dance').click(function() {
+		$('#info').show();
+		$('#staff').hide();
+document.getElementById("intro").innerHTML = "肥宅在跳舞 <br>
+整個世界都要跟著他的grooving <br>
+所以我們先跳50% 先跳50%<br>
+ 當肥宅在跳舞 <br>
+朋友啊 <br>
+在你身後的 <br>
+不是地震 是資訊之夜 大一舞<br>";
+document.getElementById("performer").innerHTML = "
+呂英弘,楊舒媛,陳允中,周忠毅<br>
+周良冠,李穆凭,曾千育,陳昱儒<br>
+廖莉祺,吳孟軒,陳弘梵,賴建穎<br>
+鐘偉傑,孫凡耘,張凱捷,羅凱齡<br>
+楊聖斌,朱柏澄,鍾承睿,毛鈺翔<br>
+甯芝蓶,謝宏祺,王郁婷,許家豪<br>
+楊舒瑄,施長元
+";
+	});
+
+	$('#2drama').click(function() {
+		$('#info').show();
+		$('#staff').hide();
+document.getElementById("intro").innerHTML = "
+  「得按摩店者得天下」迪佛勒羅德如是說。<br>
+「天下只有媽媽好，有媽的孩子像個寶」翻譯官附和著。<br>
+「你說什麼？」黑道老大凶狠地瞪著他們，氣勢驚人。他最喜歡媽媽了，沒有人能阻止他。<br>
+迪佛勒羅德 X 黑道老大。霸氣登場
+";
+document.getElementById("performer").innerHTML = "
+江昶翰<br> 
+郭冠宏<br>
+廖廷浩<br>
+賴昱憲<br>
+張庭維<br>
+邵楚荏<br>
+紀典佑<br>
+楊力權<br>
+吳克駿<br>
+周家宇<br>
+";
+	});
+
+	$('#break').click(function() {
+		$('#info').show();
+		$('#staff').hide();
+document.getElementById("intro").innerHTML = "會長交接";
+document.getElementById("performer").innerHTML = "
+會長
+";
+	});
+	
+	$('#2fork').click(function() {
+		$('#info').show();
+		$('#staff').hide();
+document.getElementById("intro").innerHTML = "
+		「家謙的戀愛學（下）」 <br>
+		我是家謙 <br>
+		大家都叫我柯基 <br>
+		我天天喝咖啡 <br>
+		最近開始十點睡七點起床 <br>
+		每天睡滿九小時 <br>
+		精神抖擻 <br>
+		大概要這樣才能當學霸吧 <br>
+		第一次寫android花了兩天就寫了2048 <br>
+		我覺得拉<br>
+		以後一個月賺250K差不多<br>
+		然後啊<br>
+		彈琴真的是賺不到這麼多<br>
+		所以我現在一天只彈15分鐘<br>
+		但我覺得這樣就夠了<br>
+		我還是有在進步<br>
+		而且我覺得我以後一定賺250K<br>
+		雖然我輔了哲學系<br>
+		但這學期一堂哲學系的課也沒修到<br>
+		不過哲學真的是蠻有趣的<br>
+		我也常在思考我的生活<br>
+		感覺真的很棒<br>
+		至於什麼女朋友的 算了<br>
+		等賺250K之後再說好了<br>
+		就跟我覺得我抽獎每次都會中一樣<br>
+		我一定會有女朋友的<br>
+";
+document.getElementById("performer").innerHTML = "
+		邵楚荏<br>
+		林煦恩<br>
+		吳家謙<br>
+		林書瑾<br>
+		鄭德馨<br>
+		陳雋<br>
+";
+	});
+	
+	$('#olddance').click(function() {
+		$('#info').show();
+		$('#staff').hide();
+document.getElementById("intro").innerHTML = "
+		老人舞沒有介紹。 <br>
+";
+document.getElementById("performer").innerHTML = "
+		老人舞沒有表演人員。 <br>
+";
+	});
+	
+	$('#1drama').click(function() {
+		$('#info').show();
+		$('#staff').hide();
+document.getElementById("intro").innerHTML = "
+		趕不完的死線 手機底下微甲不曾翻過的那一頁 <br>
+		小齊齊搖頭嘆道：我選擇死亡 <br>
+		毅然決然闔上課本 關掉報告頁面 <br>
+		此時某處傳來陣陣低語....”來啊 快進來啊” <br>
+		仔細一聽 是從我的電腦裡的我的電腦裡發出來的<br>
+		在好奇心作祟下 忍不住的點開來看 <br>
+		正是那蒙上一層灰的D槽所發出的聲音 <br>
+		俗語有云：低潮與D槽 如影隨形 <br>
+		正逢低潮的齊齊當然不放過這機會 <br>
+		當他點下去的那一剎那 <br>
+		“咕魯咕魯咕魯......”<br>r>
+		齊齊竟然被吸進D槽裡了 吸的什麼也不剩 <br>
+		這 就是Lord of D 的力量 <br>
+		你準備好知道真相了嗎？ <br>
+";
+document.getElementById("performer").innerHTML = "
+		賴建穎<br> 
+		陳力榕<br>
+		謝唯安<br>
+		梁友銓<br>
+		簡宗祐<br>
+		莊翔旭<br>
+		廖莉祺<br>
+		施博瀚<br>
+		馬揚格<br>
+		林政豪<br>
+		陳家棋<br>
+		林品諺<br>
+		陳譽穆<br>
+		馮郁翔<br>
+		鄭開元<br>
+		陳昇<br>
+		吳孟軒<br>
+		朱柏澄<br>
+		何鎧至<br>
+		鄭淵仁<br>
+		張凱捷<br>
+		董書博<br>
+		王曄庭<br>
+		王立人<br>
+		王昱舒<br>
+		江緯璿<br>
+		蘇彥齊<br>
+";
+	});
+	
+	$('#2dance').click(function() {
+		$('#info').show();
+		$('#staff').hide();
+document.getElementById("intro").innerHTML = "
+		我們，大。<br>
+		我們，二。<br>
+		我們，舞。<br>
+		我們跳，大二舞。<br>
+";
+document.getElementById("performer").innerHTML = "
+		黃奕光<br>
+		紀典佑<br>
+		楊力權<br>
+		周家宇<br>
+		鄭德馨<br>
+		鄭筱樺<br>
+		林祐萱<br>
+		詹郁珍<br>
+";
+	});
+	
+	$('#oldfork').click(function() {
+		$('#info').show();
+		$('#staff').hide();
+document.getElementById("intro").innerHTML = "
+		轉眼間，四年的大學生活即將要結束了。<br>
+		下一次回過頭來，會不會又過了五年、十年？ <br>
+		四年的回憶，四年多采多姿的人生， <br>
+		會多快地忘記呢？ <br>
+		或許會不會記得不是那麼的重要。<br>
+		或許，<br>
+		有一天忘記了以前一起做過的事情， <br>
+		有一天忘記了曾經說過的話， <br>
+		甚至忘記了對方的名字， <br>
+		但永遠會記得一起經驗這四年的感覺。<br>
+";
+document.getElementById("performer").innerHTML = "
+		張鈞堯<br>
+		李承軒<br>
+		王欣維<br>
+		陳思穎<br>
+		王于青<br>
+		黃郁甯<br>
+";
+	});
+	
+	$('#elitedance').click(function() {
+		$('#info').show();
+		$('#staff').hide();
+document.getElementById("intro").innerHTML = "
+		菁英逐夢，開啟人生新旅程。<br>
+		我們不太會跳舞。<br>
+";
+document.getElementById("performer").innerHTML = "
+		陳映紅<br>
+		張天慈<br>
+		姜瓔育<br>
+		曾大祐<br>
+		陳元瑞<br>
+		陳宗涵<br>
+		黃奕光<br>
+		謝致有<br>
+		陳欣妤<br>
+		許若漢<br>
+";
+	});
+	
+	$('#yoyo').click(function() {
+		$('#info').show();
+		$('#staff').hide();
+document.getElementById("intro").innerHTML = "
+		扯鈴沒有介紹。 <br>
+";
+document.getElementById("performer").innerHTML = "
+		劉承昌<br>
+		林祥瑞<br>
+		蔡侑儒<br>
+		張文<br>
+		林宗興<br>
+		賴楷宗<br>
+		徐新凱<br>
+		李穆竺<br>
+		林碩彥<br>
+		廖名淳<br>
+		陳頤洵<br>
+";
+	});
+	
+	$('#lightdance').click(function() {
+		$('#info').show();
+		$('#staff').hide();
+document.getElementById("intro").innerHTML = "
+		今天要來說的是關於小宏的故事，而故事要從很久很久但是也沒那麼久以前開始說起，小宏從小就是一位赫赫有名的人物，他最廣為人知的就是玩弄他的那根棒棒來贏得眾人的賞識，沒有人不拜倒在他雙手精湛的技巧之下，也因此大家都稱那根棒棒為贏光棒棒。 <br>
+		有一天，小宏一如往常地在街上耍弄棒棒的時候，正當觀眾看得如癡如醉目瞪口呆垂涎三尺的同時，小宏就這樣被地上的一攤水給絆倒了，而他的贏光棒棒就這樣從他手中飛出，落入大海一去不復返。 <br>
+		先別管小宏了，你們知道在天龍國有一段淒美的愛情故事嗎？阿明從小生長在貧苦的家庭，有天當阿明出外乞討時遇見了小美，兩人四目相對，就像天雷勾動地火大火一發不可收拾，兩人很快就墜入情網，而此事很快的傳到了阿美的雙親耳中，兩人非常憤怒，要他們立刻分開，認為阿明配不上他們家的小美，而小美不顧父母的反對依然和阿明約定私奔，然而逃得了一時逃不了一世，還是被小美的父母給追到了，分開之前阿明給了小美一條項鍊，並約定五年後的這天以此項鍊相認。這些年來，小美從來沒有把項鍊離過身，就是為了提醒自己還沒有等到和阿明相遇的那一天。而五年過去了，小美在港口遲遲等不到阿明的出現，小美以為阿明不要她了，一氣之下便把項鍊給丟了，讓回憶遠走高飛。 <br>
+		但是事實並非如此，阿明這五年來無時無刻都想著小美，為了不讓自己因為身家被對方父母瞧不起，每日每夜努力讀書希望能扭轉自己的人生，終於在五年後成為了上市公司的經理，而在這天，阿明卻也怎麼樣也等不到小美，從白天等到黑夜，阿明只好失落的走了。兩人並沒有忘記彼此，原來是小美記錯地方了，這也造就了這段淒美的愛情故事。 <br>
+		好幾年過去了，正當阿明想出外釣魚散散心時，你們猜他釣到了什麼？ <br>
+";
+document.getElementById("performer").innerHTML = "
+		謝宏祺<br>
+		楊書文<br>
+		江緯璿<br>
+		陳泓為<br>
+		林品諺<br>
+		林彥昌<br>
+		林映廷<br>
+		陳奕先<br>
+		董書博<br>
+		廖廷浩<br>
+		鄭力瑋<br>
+		黃柏智<br>
+		李穆竺<br>
+		鄧逸軒<br>
+		周家宇<br>
+		郭冠宏<br>
+";
+	});
+
+        // buttons
+	$('.info_icon_hover').click(function() {
+		$('#info').toggle();
+		$('#staff').hide();
+	});
+	$('.staff_icon_hover').click(function() {
+		$('#staff').toggle();
+		$('#info').hide();
+	});
+
+	$(".info_icon").mouseenter(function() {
+		$(".info_icon_hover").show();
+		$(".info_icon").hide();
+		$(this).hide();
+	});
+	$(".info_icon_hover").mouseout(function() {
+		$(".info_icon").show();
+		$(".info_icon_hover").hide();
+		$(this).hide();
+	});
+
+	$(".staff_icon").mouseenter(function() {
+	    $(".staff_icon_hover").show();
+	    $(".staff_icon").hide();
+	    $(this).hide();
+	});
+	$(".staff_icon_hover").mouseout(function() {
+		$(".staff_icon").show();
+		$(".staff_icon_hover").hide();
+		$(this).hide();
+	});
+
+
+	//tabs
+    $(function(){
+		var _showTab = 0;
+		var $tab = $(this); 
+		var $defaultLi = $('.tag', $tab).eq(_showTab).addClass('active');
+		$($defaultLi.find('a').attr('href')).siblings().hide();
+		$('.tag', $tab).click(function() {
+			var $this = $(this),
+				_clickTab = $this.find('a').attr('href');
+			$this.addClass('active').siblings('.active').removeClass('active');
+			$(_clickTab).stop(false, true).fadeIn().siblings().hide();
+			return false;
+		}).find('a').focus(function(){
+			this.blur();
+		});
+	});
+
+});
